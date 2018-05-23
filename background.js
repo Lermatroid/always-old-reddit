9377b7a2f5dd97bf9ba87ae6c04020cca103a9ba
Tech Hax <38444224+TechHax@users.noreply.github.com>
2018-05-23 16:59:57 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+var host = "https://old.reddit.com/";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://reddit.com/*",
+            "*://www.reddit.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
